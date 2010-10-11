4c86abbc6e0161bfddebd28323bd4ea24dbb6376
Dan Barber <danbee@gmail.com>
2010-10-11 08:32:40 -0400

    Added all the missing files that weren't there.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photos::Application.config.secret_token = 'd1211d350d798302a7221c5278ead687c890f694207a9f6b5224a26bac2f00e853f9ecc780554d2e3f1f36606a583c7c6f1f371ce945082f7a3154132762e1b7'
