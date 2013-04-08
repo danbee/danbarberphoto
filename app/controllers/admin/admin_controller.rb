c82b7bd58b6cb021bcb99a86aafa4f5649278941
Dan Barber <danbee@gmail.com>
2013-04-08 17:17:41 +0100

    Force SSL for admin and redirect to configured host.

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,7 +2,9 @@ class Admin::AdminController < ApplicationController
   layout 'admin/layouts/admin'
   before_filter :authenticate_admin_user!
   before_filter :admin_menu
-  
+
+  force_ssl :host => APP_CONFIG[:ssl_hostname]
+
   def admin_menu
     @admin_menu = { :dashboard => '',
                     :admin_users => '',
