f4a02ad3797715fad09f3356cd6d95b6e7370ef8
Dan Barber <danbee@gmail.com>
2013-05-30 09:06:08 +0100

    Add 404 page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
   def after_sign_in_path_for(resource_or_scope)
     admin_dashboard_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
     new_admin_user_session_path
+  end
+
+  def render_404
+    render 'errors/not_found', :status => 404
   end
 
 end
