9aceebfe531ef720b00eef2f03eecc634600d150
Dan Barber <danbee@gmail.com>
2013-06-05 22:18:50 +0100

    Add spec for email sending.

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :contact do
+    email "test@danbarberphoto.com"
+    name "Dan Barber"
+    message "This is a message."
+  end
+end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -7,4 +7,12 @@ describe Contact do
 
   it { should allow_value("test@test.com").for(:email) }
   it { should_not allow_value("test@test").for(:email) }
+
+  let(:contact) { build(:contact) }
+
+  it "should send an email" do
+    contact.save
+    expect(ActionMailer::Base.deliveries.last.from).to eql([contact.email])
+    expect(ActionMailer::Base.deliveries.last.to).to eql(['enquiries@danbarberphoto.com'])
+  end
 end
