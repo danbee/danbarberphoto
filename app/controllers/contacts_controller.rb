4759cb4f4348fbfb9a6edbd82320098e8de9ad92
Dan Barber <dan@new-bamboo.co.uk>
2014-03-05 17:45:46 +0000

    Remove permitted params on contact controller.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,18 +5,12 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(permitted_params)
+    @contact = Contact.new(params[:contact])
     if @contact.save
       redirect_to(:new_contact, :notice => t("contact.thanks"))
     else
       flash[:alert] = t("contact.invalid")
       render :new
     end
-  end
-
-  private
-
-  def permitted_params
-    params.require(:contact).permit(:id, :email, :subject, :name, :message)
   end
 end
