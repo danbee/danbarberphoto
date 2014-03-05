34649b92ad234e41134b9e8a205a999c9cd91a7b
Dan Barber <dan@new-bamboo.co.uk>
2014-03-05 17:44:54 +0000

    Re-add accessors for contact model. (D'oh.)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,6 +5,8 @@ class Contact
 
   validates :email, :email => true
 
+  attr_accessor :id, :email, :subject, :name, :message
+
   def initialize(attributes = {})
     attributes.each do |key, value|
       self.send("#{key}=", value)
