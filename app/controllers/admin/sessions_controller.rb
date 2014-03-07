34baa4bb48bdfccb20fcab866b8be8b5f6515b68
Dan Barber <dan@new-bamboo.co.uk>
2014-03-07 10:53:09 +0000

    Add warning if not on an SSL connection.

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,3 +1,12 @@
 class Admin::SessionsController < ::Devise::SessionsController
   layout "admin/layouts/login"
+
+  before_filter :check_https
+
+  private
+
+  def check_https
+    secure_link = url_for(protocol: 'https')
+    flash[:alert] = I18n.t('insecure_alert', secure_link: secure_link).html_safe unless request.scheme == 'https'
+  end
 end
