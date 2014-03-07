f5fb7426b909f360f6b57ed836b6a532e87fb48d
Dan Barber <dan@new-bamboo.co.uk>
2014-03-07 16:00:03 +0000

    Add image fixture and validation.

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
   factory :photo do
     title "A Photo"
     description "A lovely photo of a tree"
+    image Rails.root.join('spec/fixtures/photo.jpg')
   end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe Photo do
   it { should have_and_belong_to_many(:categories) }
 
+  it { should validate_presence_of(:image) }
+
   let(:photo) { create(:photo) }
 
   it "logs a view" do
