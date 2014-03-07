cbf09ee37a964374aab7f5a79077b62980eb3b35
Dan Barber <dan@new-bamboo.co.uk>
2014-03-07 16:00:59 +0000

    Remove paperclip gem and model definition.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,27 +2,9 @@ class Photo < ActiveRecord::Base
 
   has_and_belongs_to_many :categories
 
-  has_attached_file :photo, styles: { preview: '600x600',
-                                      size17: '476x476#',
-                                      size11: '308x308#',
-                                      size8: '224x224#',
-                                      size5: '140x140#',
-                                      size3: '84x84#',
-                                      size2: '56x56#',
-                                      size17x2: '952x952#',
-                                      size11x2: '616x616#',
-                                      size8x2: '448x448#',
-                                      size5x2: '280x280#',
-                                      size3x2: '168x168#',
-                                      size2x2: '112x112#' },
-                            storage: :s3,
-                            s3_credentials: "#{Rails.root}/config/s3.yml",
-                            s3_protocol: 'https',
-                            path: ':attachment/:id/:style.:extension',
-                            bucket: ENV['AWS_BUCKET_NAME'],
-                            url: ':s3_domain_url'
-
   dragonfly_accessor :image
+
+  validates :image, presence: true
 
   @@per_page = 11
 
