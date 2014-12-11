4ae2153578ca046dcece685fe03eb9207e83f052
Dan Barber <hello@danbarber.me>
2014-12-11 13:16:24 +0000

    Refactor.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -6,10 +6,7 @@ class AdminUser < ActiveRecord::Base
 
   # new function to set the password without knowing the current password used in our confirmation controller.
   def attempt_set_password(params)
-    p = {}
-    p[:password] = params[:password]
-    p[:password_confirmation] = params[:password_confirmation]
-    update_attributes(p)
+    update_attributes(params.slice(:password, :password_confirmation))
   end
 
   # new function to return whether a password has been set
@@ -19,7 +16,7 @@ class AdminUser < ActiveRecord::Base
 
   # new function to provide access to protected method unless_confirmed
   def only_if_unconfirmed
-    unless_confirmed {yield}
+    unless_confirmed { yield }
   end
 
   def password_required?
