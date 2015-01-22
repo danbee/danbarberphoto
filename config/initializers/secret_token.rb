31f85d5ec898b19a3fd679da152733c0cd8379e6
Dan Barber <hello@danbarber.me>
2015-01-22 16:19:55 +0000

    Fix stupid secret_key_base vulnerability.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DanBarberPhoto::Application.config.secret_token = '61b03777f23e7feb0610d1a7ef5f89c7cf884b73502ab4ef88517d578308a8adeddbf5b362776b1d48957a0b4f3d2227208b62ba057666b90eec6cd804a71a03'
-DanBarberPhoto::Application.config.secret_key_base = '9d78ea3f64fe5c72becfe6abb21613efbcef98a85749bca94b57dac24f9087efff584aae98f4f8ce7a38eedb0507f48eaf0ab7580a63ca3f47091846047f3e71'
+DanBarberPhoto::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
