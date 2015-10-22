81a2150cc1452d6396297e542c0316f88c559bad
Dan Barber <github@danbarber.me>
2015-10-22 18:36:31 +0100

    Add truncate method

--- lib/administrate/fields/markdown.rb
+++ lib/administrate/fields/markdown.rb
@@ -5,10 +5,18 @@ module Administrate
         renderer.render(data).html_safe
       end
 
+      def truncate
+        data.to_s[0...truncation_length]
+      end
+
       private
 
       def renderer
         Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions: {})
+      end
+
+      def truncation_length
+        options.fetch(:truncate, 50)
       end
     end
   end
