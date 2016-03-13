35c0345c2b222dea598a0f37ad6651204829f253
Dan Barber <github@danbarber.me>
2016-03-13 18:59:48 +0000

    Move view logging specs to new views controller

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -7,19 +7,4 @@ describe PhotosController, type: :controller do
       expect(response).to render_template(:index)
     end
   end
-
-  describe 'GET log_view' do
-    let(:photo) { create(:photo) }
-
-    it 'logs a photo view' do
-      expect_any_instance_of(Photo).to receive(:log_view).once
-      get :log_view, id: photo.id
-      expect(response).to be_successful
-    end
-
-    it 'responds with not_found if the photo is not present' do
-      get :log_view, id: 999
-      expect(response.status).to eql(404)
-    end
-  end
 end
---
+++ spec/controllers/views_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe ViewsController, type: :controller do
+  describe 'POST create' do
+    let(:photo) { create(:photo) }
+
+    it 'logs a photo view' do
+      expect_any_instance_of(Photo).to receive(:log_view).once
+      post :create, photo_id: photo.id
+      expect(response).to be_successful
+    end
+
+    it 'responds with not_found if the photo is not present' do
+      post :create, photo_id: 999
+      expect(response.status).to eql(404)
+    end
+  end
+end
